f2ca324aad4094542a19e56c21b1791a259bea1c
Willbeckh <wileyhndirangu04@gmail.com>
2022-03-06 01:35:39 +0300

    add form validation for date picker

--- js/script.js
+++ js/script.js
@@ -40,19 +40,20 @@ generateButton.addEventListener("click", (e) => {
 let generateName = () => {
   const userGenderInput = document.querySelectorAll(
     "input[name='gender']:checked"
-  );
+  ); // gets checked option of radio input
 
   const userDateInput = document.querySelector("#date").value;
-  let weekday = new Date(userDateInput); // convert form date to date obj
+  let weekday = new Date(userDateInput); // convert from string to date obj
   let day = weekday.getDay();
   // console.log(day);
 
   // loop thru object and match name
-  // console.log(typeof userGenderInput);
   userGenderInput.forEach((choice) => {
     let nameGenerator = () => {
-      if (choice.value == null) return choice.value;
-      if (choice.value === "male")
+      let userChoice = choice.value; // stores the checked value
+
+      if (userChoice === null) return userChoice; // prevent empty choices of gender
+      if (userChoice === "male")
         return `Your akan name is ${APP_DATA.maleAkanNames[day]} and u were born on a ${APP_DATA.days[day]}`;
       return `Your Akan name is ${APP_DATA.femaleAkanNames[day]} and u were born on a ${APP_DATA.days[day]}`;
     };
