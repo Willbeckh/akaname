b3fd01f88c7aadfc4b4ae8463c4de5cd810ce0d6
Willbeckh <wileyhndirangu04@gmail.com>
2022-03-07 18:44:23 +0300

    fix form data validation

--- js/script.js
+++ js/script.js
@@ -1,63 +1,51 @@
 "use strict";
 
 // collect data from html
-const generateButton = document.querySelector(".generate-btn");
-const displayName = document.querySelector(".name-container");
+// const generateButton = document.querySelector(".generate-btn");
+const formData = document.querySelector("#form");
+const displayName = document.querySelector(".akan-name");
 
-const APP_DATA = {
-  days: [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ],
+const days = [
+  "Sunday 😇",
+  "Monday 🙂",
+  "Tuesday 😉",
+  "Wednesday 🤩",
+  "Thursday 😂",
+  "Friday 😛",
+  "Saturday 😎",
+];
 
-  maleAkanNames: [
-    "Kwasi",
-    "Kwadwo",
-    "Kwabena",
-    "Kwaku",
-    "Yaw",
-    "Kofi",
-    "Kwame",
-  ],
+const akanNames = {
+  male: ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"],
 
-  femaleAkanNames: ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"],
+  female: ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"],
 };
-
-// // button click event
-generateButton.addEventListener("click", (e) => {
-  e.preventDefault();
-
-  // call generate names function
-  createName();
-});
 
 // Name generation.
-let createName = () => {
-  const userGenderInput = document.querySelectorAll(
+const validateData = (submit) => {
+  submit.preventDefault();
+
+  const date = document.querySelector("#date").value;
+  if (date === "") return alert("No date entered!");
+
+  const userDateInput = new Date(date);
+  if (new Date() - userDateInput < 0)
+    return alert(
+      "Oops, Looks like you're a time traveller.🙃 provide a date past today."
+    );
+
+  const userGenderInput = document.querySelector(
     "input[name='gender']:checked"
-  ); // gets checked option of radio input
+  );
 
-  const userDateInput = document.querySelector("#date").value;
-  let weekday = new Date(userDateInput); // convert from string to date obj
-  let day = weekday.getDay();
-  // console.log(day);
+  if (userGenderInput === null) return alert("Please select Gender.");
 
-  // loop thru object and match name
-  userGenderInput.forEach((choice) => {
-    let nameGenerator = () => {
-      let userChoice = choice.value; // stores the checked value
+  const day = userDateInput.getDay();
+  createAkanName(userGenderInput.value, day);
 
-      if (userChoice === null); // prevent empty choices of gender
-      // if (userChoice === "male")
-        displayName.innerText =  `Your akan name is ${APP_DATA.maleAkanNames[day]} and u were born on a ${APP_DATA.days[day]}`;
-      displayName.innerText =  `Your Akan name is ${APP_DATA.femaleAkanNames[day]} and u were born on a ${APP_DATA.days[day]}`;
-    };
-    console.log(nameGenerator());
-  });
+  formData.reset();
 };
 
+const createAkanName = (gender, dayOfWeek) => akanNames[gender][dayOfWeek];
+
+formData.addEventListener("submit", validateData);
